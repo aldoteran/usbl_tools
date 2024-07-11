091d27f028e3b1f380cd2a24f1c831f00fba8134
Aldo Teran <aldot@kth.se>
2024-07-11 14:06:48 +0200

    Update usbl_interface_tester to include checksum of sent message

--- scripts/usbl_interface_tester.py
+++ scripts/usbl_interface_tester.py
@@ -4,19 +4,30 @@ import rospy
 from std_msgs.msg import Char
 from std_msgs.msg import Float64
 
+import time
+
 def main():
     rospy.init_node("usbl_interface_tester")
 
     usbl_pub = rospy.Publisher("lolo/core/usbl/received", Char, queue_size=100)
     depth_pub = rospy.Publisher("lolo/dr/depth", Float64, queue_size=1)
 
-    msg = "RUNCMD 723486132 1 2 3 4 5\n"
+    msg = "RUNCMD ~/./usbl_stop.sh"
+    stamp = int(time.time())
+    msg = msg + " " + str(stamp)
+
+    bytelist = [ord(c) for c in msg]
+    checksum = hex(sum(bytearray(bytelist)) % 256)[2:]
+
+    msg = msg + " " + checksum + "\n"
+
+    # msg = "REL 1 2 3 4\n"
 
     while not rospy.is_shutdown():
         depth_pub.publish(Float64(15.0))
+        rospy.loginfo("Relaying message: {0}.".format(msg))
         for c in msg:
             usbl_pub.publish(Char(ord(c)))
-        rospy.loginfo("Relaying message.")
         rospy.sleep(3)
 
 if __name__ == "__main__":
